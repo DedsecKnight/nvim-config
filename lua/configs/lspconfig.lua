214ca97445a8091af571f80e83ca4d92b5359002
DedsecKnight <mikenguyen7268@gmail.com>
2024-08-07 03:09:57 -0500

    added pyright

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "tsserver", "emmet_ls" }
+local servers = { "html", "cssls", "tsserver", "emmet_ls", "pyright" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
