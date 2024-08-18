7e3c79ce1e80c47c8c16a0efc8e167d625700478
DedsecKnight <mikenguyen7268@gmail.com>
2024-08-17 23:28:09 -0500

    fixed commenting for react component

--- lua/custom/plugins/comments.lua
+++ lua/custom/plugins/comments.lua
@@ -1,17 +1,14 @@
 return {
+  { 'JoosepAlviste/nvim-ts-context-commentstring' },
   {
     'numToStr/Comment.nvim',
     opts = {},
     config = function()
-      require('Comment').setup()
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
       vim.keymap.set('v', '<C-_>', 'gc', { desc = 'Comment', remap = true })
       vim.keymap.set('n', '<C-_>', 'gcc', { desc = 'Comment', remap = true })
     end,
-  },
-  {
-    'folke/ts-comments.nvim',
-    opts = {},
-    event = 'VeryLazy',
-    enabled = vim.fn.has 'nvim-0.10.0' == 1,
   },
 }
