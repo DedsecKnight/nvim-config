c1edbddf5edbc655b39f58480414fe460033f70f
DedsecKnight <mikenguyen7268@gmail.com>
2024-08-19 22:04:57 -0500

    reconfigured cmdline_popup to make it look better

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -80,6 +80,13 @@ return {
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false, -- add a border to hover docs and signature help
       },
+      views = {
+        cmdline_popup = {
+          win_options = {
+            winhighlight = 'NormalFloat:NormalFloat,FloatBorder:FloatBorder',
+          },
+        },
+      },
     },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
